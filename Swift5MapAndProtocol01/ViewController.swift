878ab8cae5ec39ab305d0c1886ec82d5417807f7
Fujio Nakatsuka <com.nifty.cbm01799>
2020-08-17 20:42:40 +0900

    Initial Commit

---
+++ Swift5MapAndProtocol01/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift5MapAndProtocol01
+//
+//  Created by 中塚富士雄 on 2020/08/17.
+//  Copyright © 2020 中塚富士雄. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
